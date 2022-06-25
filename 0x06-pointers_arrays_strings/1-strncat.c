8421fd7429ae77dae2295d04851b2f07c6b956ac
Chrispel5 <ekundayopelumi95@gmail.com>
2022-06-25 09:31:08 -0700

    return (add_strings(n1, n2, r, --size_r));

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * _strncat function is similar to the _strcat function, except that
- * it will use at most n bytes from src; and
- * src does not need to be null-terminated if it contains n or more bytes
- * Return a pointer to the resulting string dest
+ * _strncat - Concatenates two strings using at most
+ * an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int s1 = 0, s2 = 0;
-	
-	while (dest[s1])
-		s2++;
-	
-	for (s1 = 0; src[s1] && s2 < n; s1++)
-		dest[s2++] = src[s1];
-	
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
